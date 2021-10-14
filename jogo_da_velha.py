cb1d1d7c45f7cb9e7eb769965514f85187a2bae0
mma <mateusmantoan@gmail.com>
2021-10-14 20:35:03 -0300

    modified:   jogo_da_velha.py

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -13,11 +13,13 @@ def clear():
 def print_table():
     clear()
     sleep(0.1)
+    if human == "X" or human == "O":
+        print("You chose", human, "\n")
     print(table[0]," | ",table[1]," | ",table[2],"\n--|---|--\n", table[3]," | ", table[4]," | ", table[5], \
-          "\n--|---|--\n", table[6], " | ", table[7], " | ", table[8], sep="")
+          "\n--|---|--\n", table[6], " | ", table[7], " | ", table[8], "\n", sep="")
 
 def restart_or_out():
-    x = input("Press 1 for play again, 2 for finish the game")
+    x = input("Press 1 for play again, 2 for finish the game: ")
     if x == "1":
         start()
     elif x == "2":
@@ -27,6 +29,7 @@ def restart_or_out():
         restart_or_out()
 
 def finish_game(p):
+    print_table()
     if human == p:
         print("congrats human, you win")
         restart_or_out()
@@ -36,13 +39,15 @@ def finish_game(p):
 
 def verify_win(p):
     #verify line
-    if table[0, 1, 2] == [p, p, p] or table[3, 4, 5] == [p, p, p] or table[6, 7, 8] == [p, p, p]:
+    if table[0] == p and table[1] == p and table[2] == p or table[3] == p and table[4] == p and table[5] == p \
+    or table[6] == p and table[7] == p and table[8] == p:
         finish_game(p)
     # verify collum
-    elif table[0, 3, 6] == [p, p, p] or table[1, 4, 7] == [p, p, p] or table[2, 5, 8] == [p, p, p]:
+    elif table[0] == p and table[3] == p and table[4] == p or table[1] == p and table[4] == p and table[7] == p \
+    or table[2] == p and table[5] == p and table[8] == p:
         finish_game(p)
     # verify diagonal
-    elif table[0, 4, 8] == [p, p, p] or table[2, 4, 6] == [p, p, p]:
+    elif table[0] == p and table[4] == p and table[8] == p or table[2] == p and table[4] == p and table[6] == p:
         finish_game(p)
     else:
         return None
@@ -77,9 +82,14 @@ def computer_select_field():
         else:
             table[int(f)] = computer
             verify_win(computer)
+            print_table()
 
 def start():
+    clear()
     input("Tic Tac Toe\n\nPress ENTER to start")
+    global table, empty_table, human, computer, level
+    table = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+    level = "easy"
     clear()
     choose_player()
     #choose_level()
@@ -91,16 +101,12 @@ def start():
             else:
                 computer_select_field()
     if computer == "X":
-        for i in len(table):
+        for i in range(len(table)):
             if i == 0 or i % 2 == 0:
                 computer_select_field()
             else:
                 human_select_field()
     print("Human and Computer loses")
     restart_or_out()
-
-global table, empty_table, human, computer, level
-table = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-level = "easy"
 
 start()
